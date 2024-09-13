a5393cc7796c68c550974b4546d5347b79732258
henrique <henrique.florencio307@gmail.com>
2024-09-13 10:33:42 -0300

    adaptando para produção

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { appConfig } from "./config/app.config";
 
 const app = express();
+const port = process.env.PORT || 5500;
 
 appConfig(app);
 
-app.listen(5500, () => {
+app.listen(port, () => {
   console.log("app rodando");
 });
