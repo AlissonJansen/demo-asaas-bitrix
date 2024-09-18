c522a7bb44c5fe5c0c77edfd1000f09ed5386630
henrique <henrique.florencio307@gmail.com>
2024-09-18 17:09:01 -0300

    fix: url de produção

--- src/utils/keepAlive.ts
+++ src/utils/keepAlive.ts
@@ -1,5 +1,5 @@
 export async function keepAlive() {
-  const url = "https://demo-asaas-bitrix.onrender.com";
+  const url = "https://demo-asaas-bitrix-8b6q.onrender.com";
 
   await fetch(url).catch((e) => console.log(e));
 }
