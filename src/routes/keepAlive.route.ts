8841ae6c3e76c1d852fafbe421fa7cf40a35bb89
henrique <henrique.florencio307@gmail.com>
2024-10-30 17:20:58 -0300

    feat: adição de versionamento na rota keepAlive

--- src/routes/keepAlive.route.ts
+++ src/routes/keepAlive.route.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
+import pJson from "../../package.json";
 
 const keepAliveRouter = Router();
 
 keepAliveRouter.get("/", (req: Request, res: Response) => {
-  return res.status(200).send("ok");
+  return res.status(200).send(pJson.version);
 });
 
 export default keepAliveRouter;
