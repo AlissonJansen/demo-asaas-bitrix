1d59be75b968e5918a47a5739684b951872d9fc1
henrique <henrique.florencio307@gmail.com>
2024-10-31 09:12:39 -0300

    update: testing var

--- src/utils/keepAlive.ts
+++ src/utils/keepAlive.ts
@@ -2,7 +2,7 @@ import { dotenvConfig } from "../config/env.config";
 
 export async function keepAlive() {
   const url = dotenvConfig.ASSAS.DEV_ASAAS_URL
-    ? "https://kit-notable-fish.ngrok-free.app"
+    ? "https://demo-asaas-bitrix-1.onrender.com"
     : "https://demo-asaas-bitrix-8b6q.onrender.com";
 
   await fetch(url).catch((e) => console.log(e));
